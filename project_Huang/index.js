56a959729d081b655012b25e7e7784b7f7cc010f
khienloong <khuang95@unm.edu>
2021-08-01 17:31:21 -0600

    Final2

---
+++ project_Huang/index.js
@@ -0,0 +1,216 @@
+var express = require("express");
+var router = express.Router();
+var mysql = require("mysql");
+
+//Pooling mysql connections
+const pool = mysql.createPool({
+	host: "logo2.c8v6vdi8dcei.us-east-2.rds.amazonaws.com",
+	user: "logo2",
+	password: "logologo",
+	database: "IoT_Project",
+});
+
+//Set the image time
+router.get('/time_get', (req, res, next) => {
+	var today = new Date();
+	today.setHours(today.getHours() - 6);
+
+	const json = JSON.stringify(today);
+	res.send(json);
+});
+
+router.get("/set-points_get", (req, res, next) => {
+
+	res.send("Temperature");
+
+	/*pool.getConnection((err, conn) => {
+	if(err) throw err;
+	var sql = "SELECT * FROM set_points";
+
+	conn.query(sql, (err, row) => {
+	//	conn.release();
+		if(err) throw err;
+
+		const json = JSON.stringify(row);
+		res.send(json);
+		conn.release();
+		});
+
+	}); */
+}); 
+
+
+router.get("/current_temp_get", (req, res, next) => {
+
+	pool.getConnection((err, conn) => {
+	if(err) throw err;
+	var sql = "SELECT * FROM current_temp";
+
+	conn.query(sql, (err, row) => {
+	//	conn.release();
+		if(err) throw err;
+
+		const json = JSON.stringify(row);
+		res.send(json);
+		conn.release();
+		});
+
+	});
+});
+
+
+router.get("/Action_get", (req, res, next) => {
+
+	pool.getConnection((err, conn) => {
+	if(err) throw err;
+	var sql = "SELECT * FROM Action";
+
+	conn.query(sql, (err, row) => {
+	//	conn.release();
+		if(err) throw err;
+
+		const json = JSON.stringify(row);
+		res.send(json);
+		conn.release();
+		});
+
+	});
+});
+
+
+//Miguel's help, do for each table
+router.post("/set-point_post", (req, res, next) => {
+
+	var time1 = req.body.time1;
+	var temp1 = req.body.temp1;
+	var temp2 = req.body.temp2;
+
+	pool.getConnection((err, conn) => {
+	if(err) throw err;
+
+		var sql = "UPDATE set_points SET time = '" + time1 + "', temp1 = '" + temp1 + "', temp2 = '" + temp2;"')"
+		conn.query(sql, (err, result) => {
+		if(err) res.send("\r\n Couldn't update set_points\r\n");
+		res.send("data updated");
+		conn.release();
+		});
+	
+	    if(err) throw err;
+
+	    var sql = "SELECT * FROM set_points"; //selects all data in table
+	   // var sql = "SELECT * FROM set_points WHERE id = 1"; //gets a row from the table
+	   // var sql = "SELECT time, temp1, temp2 FROM set_points"; //Gives specific colums for whole table
+	   // var sql = "SELECT time, FROM set_points WHERE id = 1"; //Grabs specific column for a specific row
+
+	    conn.query(sql, (err, result) =>
+	    { 
+		    if(err) throw err;
+		    var rows = JSON.stringify(result);
+		    res.send(rows);
+
+	     });
+    });
+}); 
+
+//Do one of these for each of the tables
+router.post("/current_temp_post", (req, res, next) =>{
+	
+	var time = req.body.time;
+	var temp = req.body.temp;
+	var status = req.body.status;
+
+	pool.getConnection((err, conn)=> {
+	if(err) throw err;
+		
+	var sql = "INSERT INTO current_temp (time, temp, status) VALUES ('"+time+"',"+temp+",'"+status+"')";
+		conn.query(sql, (err, result) => {
+		if(err) throw err;
+		res.send('Record inserted');
+		conn.release();
+		});	
+});
+});
+
+router.post("/set_points_post", (req, res, next) =>{
+	
+	var temp1 = req.body.temp1;
+	var temp2 = req.body.temp2;
+	var time1 = req.body.time1;
+	var time2 = req.body.time2;
+	var time3 = req.body.time3;
+
+	pool.getConnection((err, conn) => {
+	if(err) throw err;
+		
+		var sql = "INSERT INTO set_points(temp1, temp2, time1, time2, time3) VALUES('"+temp1+"','"+temp2+"','"+time1+"','"+time2+"','"+time3+"')";
+		
+		conn.query(sql, (err, result) => {
+		if(err) throw err;
+		res.send('Record inserted');
+		conn.release();
+		});
+	});
+});
+
+router.post("/Action_post", (req, res, next) =>{
+	
+	var time = req.body.time;
+	var status = req.body.status;
+
+ 
+	pool.getConnection((err, conn) => {
+	if(err) throw err;
+		
+		var sql = "INSERT INTO Action (time, status) VALUES ('"+time+"','"+status+"')";
+		
+		conn.query(sql, (err, result) => {
+		if(err) throw err;
+		res.send('Record inserted');
+		conn.release();
+		});
+	});
+});
+
+
+router.post("/", (req, res, next) => {
+
+	var temp = req.body.str;
+
+	pool.getConnection((err, conn) => {
+		if (err) throw err;
+		var sql = "Select * from set_points";
+		var heater_status
+		conn.query(sql, (err, result) => {
+			if (err) throw err;
+			var rows = JSON.parse(JSON.stringify(result));
+			var temp1 = rows[0]["temp1"];
+			var temp2 = rows[0]["temp2"];
+			var time1 = rows[0]["time1"];
+			var time2 = rows[0]["time2"];
+			var time3 = rows[0]["time3"];
+			temp1 = parseFloat(temp1);
+			temp2 = parseFloat(temp2);
+
+
+			var today = new Date();
+			today.setHours(today.getHours() - 6);
+
+			if (temp < temp1) {
+				heater_status = "ON";
+			};
+
+			if (temp > temp2) {
+				heater_status = "OFF";
+			};
+
+			heater_status = JSON.stringify(heater_status);
+			res.send(heater_status);
+
+		});
+
+	});
+
+});
+
+
+module.exports = router;
---
+++ project_Huang/proj_daemon.c
@@ -0,0 +1,269 @@
+//Compiled with Desktop/buildroot-2021.05/output/host/bin/arm-linux-gcc 
+
+#define _POSIX_SOURCE
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+//#include <stdbool.h> commented out to harden code
+#include <signal.h>
+#include <stdio.h>
+#include <syslog.h>
+#include <string.h>
+#include <time.h>
+#include <curl/curl.h>
+#undef _POSIX_SOURCE
+
+#define OK	   1
+#define ERR_CHDIR  2
+#define ERR_SETSID 3
+#define ERR_FORK   4
+#define	ERR_WTF    5   
+#define DAEMON_NAME "sampled"
+#define ERROR_FORMAT "%s"
+
+#define URL "http://ec2-18-117-105-80.us-east-2.compute.amazonaws.com:3000/"
+#define set_points_get "http://ec2-18-117-105-80.us-east-2.compute.amazonaws.com:3000/set-points_get"
+#define current_temp_get "http://ec2-18-117-105-80.us-east-2.compute.amazonaws.com:3000/current_temp_get"
+#define action_get "http://ec2-18-117-105-80.us-east-2.compute.amazonaws.com:3000/Action_get"
+#define set_points_post "http://ec2-18-117-105-80.us-east-2.compute.amazonaws.com:3000/set-point_post"
+#define current_temp_post "http://ec2-18-117-105-80.us-east-2.compute.amazonaws.com:3000/current_temp_post"
+#define action_post "http://ec2-18-117-105-80.us-east-2.compute.amazonaws.com:3000/Action_post"
+
+static void _signal_handler(const int signal) {
+  switch(signal) {
+    case SIGHUP:
+      break;
+    case SIGTERM:
+      syslog(LOG_INFO, "received SIGTERM, exiting.");
+      closelog();
+      exit(OK);
+      break;
+    default:
+      syslog(LOG_INFO, "received unhandled signal");
+  }
+}
+
+void read_callback(char* buffer, size_t itemSize, size_t nItems, void* ignore)
+{
+    FILE* temp_ptr;
+    //size_t bytes = itemSize * nItems;
+    temp_ptr = fopen("/var/log/status", "w");
+    fprintf(temp_ptr, "%s", buffer);
+    fclose(temp_ptr);
+    syslog(LOG_INFO, "Status is %s", buffer);
+   
+}
+void doPost(char* myString)
+{
+    CURL* curl;
+    CURLcode res;
+    char str[4] = "str=";
+    char message[100];
+    strcat(message, str);
+    strcat(message, myString);
+    
+    curl = curl_easy_init();
+    if (curl) {
+
+        curl_easy_setopt(curl, CURLOPT_URL, URL);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, message);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, read_callback);
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+
+        curl_easy_cleanup(curl);
+    }
+    curl_global_cleanup();
+
+
+}
+
+void doGet(char* url)
+{
+    curl_global_init(CURL_GLOBAL_ALL);
+
+    char* data;
+    CURL* curl;
+    CURLcode res;
+
+    curl = curl_easy_init();
+
+    if (curl)
+    {
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+
+        //curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+        //curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, data);
+
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+
+
+        curl_easy_cleanup(curl);
+    }
+}
+
+size_t data(char* buffer, size_t itemSize, size_t nItems, void* ignore)
+{
+    FILE* temp_ptr;
+    size_t bytes = itemSize * nItems;
+    temp_ptr = fopen("/var/log/project", "w");
+    fprintf(temp_ptr, "%s", buffer);
+    fclose(temp_ptr);
+    syslog(LOG_INFO, buffer);
+    return bytes;
+
+}
+
+//function to read from temperature file
+char* read_temp()
+{
+    
+    FILE* temperature;
+    char* buff;
+
+    temperature = fopen("/var/log/temp", "r");
+    fscanf(temperature, "%s", buff);
+    fclose(temperature);
+    syslog(LOG_INFO, "The temperature is %s", buff);
+    return buff;
+
+}
+
+
+static void _do_work(void) {
+  time_t time_current;
+ struct tm* chrono_counter;
+  FILE *fptr;
+
+  int current_temp;
+  char * buffer = 0;
+  long length;
+
+  char* action_id;
+  char* action_status;
+  char* action_time;
+
+  char* set_points_id;
+  char* set_points_temp1;
+  char* set_points_temp2;
+  char* set_points_time1;
+  char* set_points_time2;
+  char* set_points_time3;
+
+  char* current_temp_id;
+  char* current_temp_temp;
+  char* current_temp_status;
+  char* current_temp_time;
+
+  int set_tmp;
+  char* hrs_morning;
+  int morning_int;
+  int min_morning;
+
+  char* temp;
+
+  while(1==1)
+  {
+      doGet(set_points_get);
+      syslog(LOG_INFO, "This works!");
+      
+     /*temp =*/ read_temp();
+      //doPost(temp);
+
+      /*fptr = fopen("/var/log/project", "rb");
+      fseek(fptr, 0, SEEK_END);
+      length = ftell(fptr);
+      fseek(fptr, 0, SEEK_SET);
+      buffer = malloc (length);
+      if (buffer)
+      {
+          fread(buffer, 1, length, fptr);
+      }
+      fclose(fptr);
+      syslog(LOG_INFO, "%s", buffer); 
+
+      set_points_id = strtok(buffer, " ");
+      set_points_temp1 = strtok(NULL, " ");
+      set_points_temp2 = strtok(NULL, " ");
+      set_points_time1 = strtok(NULL, " ");
+      set_points_time2 = strtok(NULL, " ");
+      set_points_time3 = strtok(NULL, " ");
+      syslog(LOG_INFO, "The first bit of data: id:%s temp1:%s temp2:%s time1:%s time2:%s time3%s", set_points_id, set_points_temp1, set_points_temp2, 
+          set_points_time1, set_points_time2, set_points_time3);
+      set_tmp = atoi(set_temp_str);
+      temp_morning = atoi(set_temp_str);
+
+      hrs_morning = strtok(set_time, ":");
+      morning_int = //see whatsapp
+      
+
+    time(&time_current);
+    chrono_counter = localtime(&time_current);
+    syslog(LOG_INFO, "current time: %i:%i:%i\n",chrono_counter->tm_hour,chrono_counter->tm_min,chrono_counter->tm_sec);
+ // for (int i = 0; true; i++) {
+ //   syslog(LOG_INFO, "iteration: %d", i);
+
+ //read from the temperature file
+fptr = fopen("/var/log/temp","r");
+fscanf(fptr,"%d",&current_temp);
+fclose(fptr);
+syslog(LOG_INFO, "current temp: %d\n",current_temp);
+
+//try to write to the heater
+fptr = fopen("/var/log/status","w");
+fprintf(fptr,"OFF : %i:%i:%i",chrono_counter->tm_hour,chrono_counter->tm_min,chrono_counter->tm_sec);
+fclose(fptr); */
+sleep(1);
+  }
+}
+
+int main(void) {
+
+   openlog(DAEMON_NAME, LOG_PID | LOG_NDELAY | LOG_NOWAIT, LOG_DAEMON);
+  syslog(LOG_INFO, "starting sampled");
+
+
+  pid_t pid = fork();
+  if (pid < 0) {
+    syslog(LOG_ERR, ERROR_FORMAT, strerror(errno));
+    return ERR_FORK;
+  }
+
+  if (pid > 0) {
+    return OK;
+  }
+
+  if (setsid() < -1) {
+    syslog(LOG_ERR, ERROR_FORMAT, strerror(errno));
+    return ERR_SETSID;
+  }
+
+
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
+
+  umask(S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+  if (chdir("/") < 0) {
+    syslog(LOG_ERR, ERROR_FORMAT, strerror(errno));
+    return ERR_CHDIR;
+  }
+
+  signal(SIGTERM, _signal_handler);
+  signal(SIGHUP,_signal_handler);
+
+
+  _do_work();
+
+  return ERR_WTF;
+
+} 
